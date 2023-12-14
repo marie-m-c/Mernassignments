1b07fba13d1c0454920e2a3d97c4c5869cd1a649
marie-m-c <cherif.mariem.777@gmail.com>
2023-12-14 11:23:00 +0100

    Add jokes assignment

---
+++ mongo_db/jokes/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json(), express.urlencoded({ extended: true }));
+
+
+require("dotenv").config()
+require("./config/mongoose.config")
+
+const port = process.env.PORT
+
+const Routes = require("./routes/joke.routes")
+Routes(app)
+
+
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`)
+})
